395289136a99db924be0e78b23aaebe1cd6695c9
KarlbomTjejerna <Malin.karlbom@svenskaspel.se>
2017-09-26 15:01:26 +0200

    class med init

--- robot.py
+++ robot.py
@@ -1,1 +1,28 @@
-print(900 * 2 * 1.25 + 100 * 1.06)
+
+  
+
+
+
+
+class Product:
+    def __init__(self, price, count, tax):
+        self.price = price
+        self.count = count
+        self.tax = tax
+
+    def price_with_tax(self):
+        return self.price * self.count * self.tax
+
+
+robot = Product(price=900, count=2, tax=1.25) 
+book = Product(price=100, count=1, tax=1.06)
+		
+
+
+
+
+
+print(robot.price_with_tax() + book.price_with_tax())
+
+
+
